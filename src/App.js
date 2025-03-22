6c6ef762b982ec01ed75a962022d8045159dd4d0
Martin Babak <martinyis11@gmail.com>
2025-03-22 14:40:09 +0100

    Add Piechart and Savings components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import './App.css';
-import BudgetBanner from './banner';
-import Salary from './salary';
-
+import "./App.css";
+import BudgetBanner from "./banner";
+import Salary from "./salary";
+import ExpensePieChart from "./components/ExpensePieChart";
+import SimpleSavingsComponent from "./components/SImpleSavingsComponent";
 function App() {
+  //fake data for now
+  const expenseData = {
+    Housing: 35,
+    Food: 20,
+    Transport: 15,
+    Entertainment: 10,
+    Medical: 5,
+    Other: 15,
+  };
+  const currentSavings = 2500;
   return (
     <div className="App">
       <BudgetBanner />
       <Salary />
+
+      <ExpensePieChart expenseData={expenseData} />
+      <SimpleSavingsComponent savings={currentSavings} />
     </div>
   );
 }
